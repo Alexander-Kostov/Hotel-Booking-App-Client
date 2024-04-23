7f190e8e6e035584fd8404edf56bb7d6733eceff
Alexander <alex3747@abv.bg>
2024-04-23 22:27:38 +0300

    fixing a button

--- lakeside-hotel-client/Lakeside-Hotel/src/App.jsx
+++ lakeside-hotel-client/Lakeside-Hotel/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react"
-import "../node_modules/bootstrap/dist/css/bootstrap/min.css"
-import "/node_modules/bootstrap/dist/js/bootstrap/min.js"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from './components/room/AddRoom'
+
 
 function App() {
  return <>
